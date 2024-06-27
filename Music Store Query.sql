6e153c2d0fd8d6f0ad6759d36209346ec67d8282
Sreeraj28kr <153048765+Sreeraj28kr@users.noreply.github.com>
2024-06-27 11:06:44 +0530

    Pizza Sales Query.sql

--- Music Store Query.sql
+++ Music Store Query.sql
@@ -1,230 +1,183 @@
 /*	Question Set 1 - Easy */
 
-/* Q1: Who is the senior most employee based on job title? */
+/* Q1: Retrieve the total number of orders placed. */
 
-SELECT title, last_name, first_name 
-FROM employee
-ORDER BY levels DESC
-LIMIT 1
-
-
-/* Q2: Which countries have the most Invoices? */
-
-SELECT COUNT(*) AS c, billing_country 
-FROM invoice
-GROUP BY billing_country
-ORDER BY c DESC
+SELECT 
+    COUNT(order_id) AS total_orders
+FROM
+    pizzahut.orders;
 
 
-/* Q3: What are top 3 values of total invoice? */
+/* Q2: Calculate the total revenue generated from pizza sales. */
 
-SELECT total 
-FROM invoice
-ORDER BY total DESC
+SELECT 
+    ROUND(SUM(orders_details.quantity * pizzas.price),2) AS total_sales
+FROM
+    pizzahut.orders_details
+        JOIN
+    pizzahut.pizzas ON orders_details.pizza_id = pizzas.pizza_id
 
 
-/* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
-Write a query that returns one city that has the highest sum of invoice totals. 
-Return both the city name & sum of all invoice totals */
+/* Q3: Identify the highest-priced pizza. */
 
-SELECT billing_city,SUM(total) AS InvoiceTotal
-FROM invoice
-GROUP BY billing_city
-ORDER BY InvoiceTotal DESC
+SELECT 
+    pizza_types.name, pizzas.price
+FROM
+    pizzahut.pizza_types
+        JOIN
+    pizzahut.pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+ORDER BY pizzas.price DESC
 LIMIT 1;
 
 
-/* Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
-Write a query that returns the person who has spent the most money.*/
+/* Q4: Identify the most common pizza size ordered. */
 
-SELECT customer.customer_id, first_name, last_name, SUM(total) AS total_spending
-FROM customer
-JOIN invoice ON customer.customer_id = invoice.customer_id
-GROUP BY customer.customer_id
-ORDER BY total_spending DESC
-LIMIT 1;
+SELECT 
+    pizzas.size,
+    COUNT(orders_details.order_details_id) AS most_ordered_size
+FROM
+    pizzahut.orders_details
+        JOIN
+    pizzahut.pizzas ON orders_details.pizza_id = pizzas.pizza_id
+GROUP BY pizzas.size
+ORDER BY most_ordered_size DESC;
+
+
+/* Q5: List the top 5 most ordered pizza types along with their quantities.*/
+
+SELECT 
+    pizza_types.name,
+    SUM(orders_details.quantity) AS total_quantity
+FROM
+    pizzahut.pizza_types
+        JOIN
+    pizzahut.pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    pizzahut.orders_details ON orders_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.name
+ORDER BY total_quantity DESC
+LIMIT 5
 
 
 
 
 /* Question Set 2 - Moderate */
 
-/* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
-Return your list ordered alphabetically by email starting with A. */
+/* Q1: Join the necessary tables to find the total quantity of each pizza category ordered. */
 
-/*Method 1 */
+SELECT 
+    pizza_types.category,
+    SUM(orders_details.quantity) AS quantity
+FROM
+    pizzahut.orders_details
+        JOIN
+    pizzahut.pizzas ON orders_details.pizza_id = pizzas.pizza_id
+        JOIN
+    pizzahut.pizza_types ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+GROUP BY pizza_types.category
+ORDER BY quantity DESC
 
-SELECT DISTINCT email,first_name, last_name
-FROM customer
-JOIN invoice ON customer.customer_id = invoice.customer_id
-JOIN invoiceline ON invoice.invoice_id = invoiceline.invoice_id
-WHERE track_id IN(
-	SELECT track_id FROM track
-	JOIN genre ON track.genre_id = genre.genre_id
-	WHERE genre.name LIKE 'Rock'
-)
-ORDER BY email;
+/* Q2: Determine the distribution of orders by hour of the day. */
 
-
-/* Method 2 */
-
-SELECT DISTINCT email AS Email,first_name AS FirstName, last_name AS LastName, genre.name AS Name
-FROM customer
-JOIN invoice ON invoice.customer_id = customer.customer_id
-JOIN invoiceline ON invoiceline.invoice_id = invoice.invoice_id
-JOIN track ON track.track_id = invoiceline.track_id
-JOIN genre ON genre.genre_id = track.genre_id
-WHERE genre.name LIKE 'Rock'
-ORDER BY email;
+SELECT 
+    HOUR(order_time) AS hours, COUNT(order_id) AS order_count
+FROM
+    pizzahut.orders
+GROUP BY hours
 
 
-/* Q2: Let's invite the artists who have written the most rock music in our dataset. 
-Write a query that returns the Artist name and total track count of the top 10 rock bands. */
+/* Q3: Join relevant tables to find the category-wise distribution of pizzas. */
 
-SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
-FROM track
-JOIN album ON album.album_id = track.album_id
-JOIN artist ON artist.artist_id = album.artist_id
-JOIN genre ON genre.genre_id = track.genre_id
-WHERE genre.name LIKE 'Rock'
-GROUP BY artist.artist_id
-ORDER BY number_of_songs DESC
-LIMIT 10;
+SELECT 
+    category, COUNT(name) AS pizza_categories
+FROM
+    pizzahut.pizza_types
+GROUP BY category
 
 
-/* Q3: Return all the track names that have a song length longer than the average song length. 
-Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. */
+/* Q4: Group the orders by date and calculate the average number of pizzas ordered per day. */
 
-SELECT name,miliseconds
-FROM track
-WHERE miliseconds > (
-	SELECT AVG(miliseconds) AS avg_track_length
-	FROM track )
-ORDER BY miliseconds DESC;
+SELECT 
+    ROUND(AVG(quantity), 0)
+FROM
+    (SELECT 
+        orders.order_date, SUM(orders_details.quantity) AS quantity
+    FROM
+        pizzahut.orders
+    JOIN pizzahut.orders_details ON orders.order_id = orders_details.order_id
+    GROUP BY orders.order_date) AS order_quantity;
+
+
+/* Q5: Determine the top 3 most ordered pizza types based on revenue. */
+
+SELECT 
+    pizza_types.name,
+    SUM(orders_details.quantity * pizzas.price) AS revenue
+FROM
+    pizzahut.pizza_types
+        JOIN
+    pizzahut.pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    pizzahut.orders_details ON orders_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.name
+ORDER BY revenue DESC
+LIMIT 3;
 
 
 
 
 /* Question Set 3 - Advance */
 
-/* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
+/* Q1: Calculate the percentage contribution of each pizza type to total revenue. */
 
-/* Steps to Solve: First, find which artist has earned the most according to the InvoiceLines. Now use this artist to find 
-which customer spent the most on this artist. For this query, you will need to use the Invoice, InvoiceLine, Track, Customer, 
-Album, and Artist tables. Note, this one is tricky because the Total spent in the Invoice table might not be on a single product, 
-so you need to use the InvoiceLine table to find out how many of each product was purchased, and then multiply this by the price
-for each artist. */
+SELECT 
+    pizza_types.category,
+    ROUND(SUM(orders_details.quantity * pizzas.price) / (SELECT 
+                    ROUND(SUM(orders_details.quantity * pizzas.price),
+                                2) AS total_sales
+                FROM
+                    pizzahut.orders_details
+                        JOIN
+                    pizzahut.pizzas ON orders_details.pizza_id = pizzas.pizza_id) * 100,
+            2) AS revenue
+FROM
+    pizzahut.pizza_types
+        JOIN
+    pizzahut.pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    pizzahut.orders_details ON orders_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.category
+ORDER BY revenue DESC;
 
-WITH best_selling_artist AS (
-	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
-	FROM invoice_line
-	JOIN track ON track.track_id = invoice_line.track_id
-	JOIN album ON album.album_id = track.album_id
-	JOIN artist ON artist.artist_id = album.artist_id
-	GROUP BY 1
-	ORDER BY 3 DESC
-	LIMIT 1
-)
-SELECT c.customer_id, c.first_name, c.last_name, bsa.artist_name, SUM(il.unit_price*il.quantity) AS amount_spent
-FROM invoice i
-JOIN customer c ON c.customer_id = i.customer_id
-JOIN invoice_line il ON il.invoice_id = i.invoice_id
-JOIN track t ON t.track_id = il.track_id
-JOIN album alb ON alb.album_id = t.album_id
-JOIN best_selling_artist bsa ON bsa.artist_id = alb.artist_id
-GROUP BY 1,2,3,4
-ORDER BY 5 DESC;
+/* Q2: Analyze the cumulative revenue generated over time. */
 
-
-/* Q2: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
-with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where 
-the maximum number of purchases is shared return all Genres. */
-
-/* Steps to Solve:  There are two parts in question- first most popular music genre and second need data at country level. */
-
-/* Method 1: Using CTE */
-
-WITH popular_genre AS 
-(
-    SELECT COUNT(invoice_line.quantity) AS purchases, customer.country, genre.name, genre.genre_id, 
-	ROW_NUMBER() OVER(PARTITION BY customer.country ORDER BY COUNT(invoice_line.quantity) DESC) AS RowNo 
-    FROM invoice_line 
-	JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
-	JOIN customer ON customer.customer_id = invoice.customer_id
-	JOIN track ON track.track_id = invoice_line.track_id
-	JOIN genre ON genre.genre_id = track.genre_id
-	GROUP BY 2,3,4
-	ORDER BY 2 ASC, 1 DESC
-)
-SELECT * FROM popular_genre WHERE RowNo <= 1
+select order_date,
+round(sum(revenue) over (order by order_date),2) as cum_revenue
+from
+(select orders.order_date,
+sum(orders_details.quantity*pizzas.price) as revenue
+from pizzahut.orders join pizzahut.orders_details
+on orders.order_id = orders_details.order_id
+join pizzahut.pizzas
+on orders_details.pizza_id = pizzas.pizza_id
+group by orders.order_date) as sales
 
 
-/* Method 2: : Using Recursive */
+/* Q3: Determine the top 3 most ordered pizza types based on revenue for each pizza category. */
 
-WITH RECURSIVE
-	sales_per_country AS(
-		SELECT COUNT(*) AS purchases_per_genre, customer.country, genre.name, genre.genre_id
-		FROM invoice_line
-		JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		JOIN track ON track.track_id = invoice_line.track_id
-		JOIN genre ON genre.genre_id = track.genre_id
-		GROUP BY 2,3,4
-		ORDER BY 2
-	),
-	max_genre_per_country AS (SELECT MAX(purchases_per_genre) AS max_genre_number, country
-		FROM sales_per_country
-		GROUP BY 2
-		ORDER BY 2)
-
-SELECT sales_per_country.* 
-FROM sales_per_country
-JOIN max_genre_per_country ON sales_per_country.country = max_genre_per_country.country
-WHERE sales_per_country.purchases_per_genre = max_genre_per_country.max_genre_number;
+select name, revenue
+from
+(select category, revenue, name,
+rank () over(partition by category order by revenue desc) as rn
+from
+(select pizza_types.category, pizza_types.name,
+sum(orders_details.quantity * pizzas.price) as revenue
+from pizzahut.pizza_types join pizzahut.pizzas
+on pizza_types.pizza_type_id = pizzas.pizza_type_id
+join pizzahut.orders_details on orders_details.pizza_id = pizzas.pizza_id
+group by pizza_types.category, pizza_types.name) as a) as b
+where rn <=3;
 
 
-/* Q3: Write a query that determines the customer that has spent the most on music for each country. 
-Write a query that returns the country along with the top customer and how much they spent. 
-For countries where the top amount spent is shared, provide all customers who spent this amount. */
-
-/* Steps to Solve:  Similar to the above question. There are two parts in question- 
-first find the most spent on music for each country and second filter the data for respective customers. */
-
-/* Method 1: using CTE */
-
-WITH Customter_with_country AS (
-		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending,
-	    ROW_NUMBER() OVER(PARTITION BY billing_country ORDER BY SUM(total) DESC) AS RowNo 
-		FROM invoice
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		GROUP BY 1,2,3,4
-		ORDER BY 4 ASC,5 DESC)
-SELECT * FROM Customter_with_country WHERE RowNo <= 1
-
-
-/* Method 2: Using Recursive */
-
-WITH RECURSIVE 
-	customter_with_country AS (
-		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending
-		FROM invoice
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		GROUP BY 1,2,3,4
-		ORDER BY 2,3 DESC),
-
-	country_max_spending AS(
-		SELECT billing_country,MAX(total_spending) AS max_spending
-		FROM customter_with_country
-		GROUP BY billing_country)
-
-SELECT cc.billing_country, cc.total_spending, cc.first_name, cc.last_name, cc.customer_id
-FROM customter_with_country cc
-JOIN country_max_spending ms
-ON cc.billing_country = ms.billing_country
-WHERE cc.total_spending = ms.max_spending
-ORDER BY 1;
-
-
-/* source: www.youtube.com/@RishabhMishraOfficial */
 
 /* Thank You :) */
